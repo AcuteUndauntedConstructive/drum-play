907441912c31c83194300ad8f40fbe1647b6e91b
Subodh Kumar Gupta <subugupta08@gmail.com>
2024-06-30 10:41:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+// eke eke button pe jake class check kar ke click hono par button jo mouse se click ho ga eventlistenr use ka html jo ki letter hai use send kar dega sound produce kar ne wale function
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        
+       var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+    });
+}
+
+
+
+// key ko sun ke kam karega
+
+document.addEventListener("keydown",function (event){
+    console.log(event.key);
+    makeSound(event.key);
+});
+
+
+// same function for both to take a value check what to do
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+                var audio = new Audio('./sounds/tom-2.mp3');
+                audio.play();
+                break;
+                case "s":
+                    var audio = new Audio('./sounds/tom-3.mp3');
+                    audio.play();
+                    break;
+                    case "d":
+                        var audio = new Audio('./sounds/tom-4.mp3');
+                        audio.play();
+                        break;
+                        case "j":
+                            var audio = new Audio('./sounds/snare.mp3');
+                            audio.play();
+                            break;
+                            case "k":
+                                var audio = new Audio('./sounds/crash.mp3');
+                                audio.play();
+                                break;
+                             case "l":
+                              var audio = new Audio('./sounds/kick-bass.mp3');
+                                 audio.play();
+                                 break;        
+
+
+        default:console.log(buttoninnerHTML);
+          
+    }
+
+}
+
+
